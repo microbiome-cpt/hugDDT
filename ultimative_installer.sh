60c99986b82d29c515364c072c1267bbf56b126f
Ilya Grabarnick <ilyagrabh@gmail.com>
2025-12-21 01:38:11 +0300

    installer script update

--- ultimative_installer.sh
+++ ultimative_installer.sh
@@ -10,25 +10,40 @@ fi
 # ensure conda shell functions
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
+echo "LETS INSTALL CONDA ENVS!"
 # --- create environments ---
 for yml in envs/*.yml; do
     mamba env create -f "$yml"
 done
 
+echo "CHECK IF THE ENVS ARE IN THE LIST:"
+conda env list
+echo ""
 
 # --- install third-party tools ---
-
-# DORADO
+echo "LETS INSTALL DORADO!"
 if ! command -v dorado &>/dev/null; then
     wget https://cdn.oxfordnanoportal.com/software/analysis/dorado-1.2.0-linux-x64.tar.gz
     tar xzf dorado-1.2.0-linux-x64.tar.gz
-    export PATH=$(pwd)/dorado-1.2.0-linux-x64/bin:$PATH
+    echo "export PATH=$(pwd)/dorado-1.2.0-linux-x64/bin:$PATH" >> "$CONDA_PREFIX/etc/conda/activate.d/dorado.sh"
+	export PATH=$(pwd)/dorado-1.2.0-linux-x64/bin:$PATH
+fi
+echo ""
+echo "CHECK IF DORADO VERSION APPEARS:"
+dorado -v
+echo ""
 
+echo "LETS INSTALL NEXTPOLISH!"
 # --- NextPolish ---
 if ! command -v nextpolish &>/dev/null; then
-    git clone https://github.com/Nextomics/NextPolish.git
-    cd NextPolish
-    make
-    cd ..
+    wget https://github.com/Nextomics/NextPolish/releases/download/v1.4.1/NextPolish.tgz
+    tar -vxzf NextPolish.tgz
     echo "export PATH=\$PATH:$(pwd)/NextPolish" >> "$CONDA_PREFIX/etc/conda/activate.d/nextpolish.sh"
+    cd NextPolish && make && cd ../
+	export PATH=$(pwd)/NextPolish:$PATH
 fi
+
+echo "DON'T BOTHER IF YOU SEE SOME [samtools] Make Error.."
+echo ""
+echo "CHECK IF NEXTPOLISH VERSION APPAERS:"
+nextPolish -v
